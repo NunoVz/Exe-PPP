b29bb9e7948b9adcf86ccaa67d55b3e7f90879c4
Vasques <76536544+NunoVz@users.noreply.github.com>
2021-02-17 15:23:13 +0000

    Create PPP.c

---
+++ PPP.c
@@ -0,0 +1,119 @@
+//
+// Created by Vasques on 17/02/2021.
+//
+#include <stdio.h>
+//Ex 1.2
+/*int main() {
+    float preco;
+    printf("Insira o preço: ");
+    scanf("%f",& preco);
+    int nota = 0;
+    int moeda = 0;
+
+    float troco = 100 - preco;
+    while (troco > 0.00) {
+        if (troco >= 50) {
+            troco -= 50;
+            nota++;
+            printf("Nota de 50\n");
+        }
+        else if (troco >= 20) {
+            troco -= 20;
+            nota++;
+            printf("Nota de 20\n");
+        }
+        else if (troco >= 10) {
+            troco -= 10;
+            nota++;
+            printf("Nota de 10\n");
+        }
+        else if (troco >= 5) {
+            troco -= 5;
+            nota++;
+            printf("Nota de 5\n");
+        }
+        else if (troco >= 2) {
+            troco -= 1;
+            moeda++;
+            printf("Moedas de 2\n");
+        }
+        else if (troco >= 1) {
+            troco -= 1;
+            moeda++;
+            printf("Moedas de 1\n");
+        }
+        else if (troco >= 0.5) {
+            troco -= 0.5;
+            moeda++;
+            printf("Moedas de 0.5\n");
+        }
+        else if (troco >= 0.2) {
+            troco -= 0.2;
+            moeda++;
+            printf("Moedas de 0.2\n");
+        }
+        else if (troco >= 0.1) {
+            troco -= 0.1;
+            moeda++;
+            printf("Moedas de 0.1\n");
+        }
+        else if (troco >= 0.05) {
+            troco -= 0.05;
+            moeda++;
+            printf("Moedas de 0.05\n");
+        }
+        else if (troco >= 0.01) {
+            troco -= 0.01;
+            moeda++;
+            printf("Moedas de 0.01\n");
+        }
+    }
+}*/
+//Ex 2.1
+/*int main() {
+    int num1,num2,num3,m,s;
+    printf("Introduza um numero:");
+    scanf("%d",& num1);
+    //2.1
+    if(num1%2){printf("O numero é impar");}
+    else{printf("O 1º numero é par");}
+    printf("Introduza um numero:");
+    scanf("%d",& num2);
+    printf("Introduza um numero:");
+    scanf("%d",& num3);
+    //2.2
+    if(num1>num2 && num1>num3){printf("%d",num1);}
+    else if(num2>num1 && num2>num3){printf("%d",num2);}
+    else if(num3>num1 && num3>num2){printf("%d",num3);}
+    //2.3
+    if(num1%2){if(num2%2){m=num1*num2;printf("%d",m);}else{printf("%d",num1);}}
+    else{if(num2%2){printf("%d",num2);}else{s=num1+num2;printf("%d",s);}}
+}*/
+//Ex2.2
+/*int main() {
+    //1
+    int idade;
+    printf("Insira a sua idade:");
+    scanf("%d",& idade);
+    if (idade>=61){printf("Isento de pagamento");}
+    else if(idade>=49){printf("Bilhete Light");}
+    else if(idade>=37){printf("Bilhete normal");}
+    else if(idade>=25){printf("Bilhete Light");}
+    else if(idade>=13){printf("Bilhete radical");}
+    else{printf("Isento de pagamento");}
+    //2
+    int hora,min,c;
+    printf("Insira as horas no formato hh:mm.\n");
+    scanf("%02d:%02d",& hora,& min);
+    if(hora>=12){c=hora-12;printf("%d:%d pm",c,min);}
+    else{printf("%d:%d am",hora,min);}
+    //3
+    int nota;
+    printf("Escreva a nota do aluno:");
+    scanf("%d",& nota);
+    if (nota<=9){printf("Insuficiente");}
+    else if (nota<=13){printf("Suficiente");}
+    else if (nota<=16){printf("Bom");}
+    else{printf("Excelente");}
+
+}*/
